8b273afbc2260d56c3b1eff329ad5d13705e53a4
RIyadhaque492 <61925524+RIyadhaque492@users.noreply.github.com>
2024-12-29 01:28:05 +0600

    file added in master

---
+++ config/constants.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+define("ROOT_URL", "http://localhost/blog/");
+define('DB_HOST', 'localhost');
+define('DB_USER', 'pma');
+define('DB_PASS', '');
+define('DB_NAME', 'blog');
+
